dbb0531a0e0328fc6b297b7ca7247d19176e1a71
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-07-16 20:22:08 +0200

    Build on wyrihaximus/to-x-or-not-to-x

--- src/functions.php
+++ src/functions.php
@@ -2,27 +2,10 @@
 
 namespace WyriHaximus;
 
-use Cake\Collection\Collection;
-use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\Reader;
 use WyriHaximus\Annotations\ChildProcess;
 
 function toChildProcessOrNotToChildProcess(string $callable, Reader $annotationReader = null): bool
 {
-    if (!($annotationReader instanceof Reader)) {
-        $annotationReader = new AnnotationReader();
-    }
-
-    list($class, $method) = explode('::', $callable);
-
-    $annotations = (new  Collection($annotationReader->getMethodAnnotations((new \ReflectionClass($class))->getMethod($method))))
-        ->indexBy(function (object $annotation) {
-            return get_class($annotation);
-        })->toArray();
-
-    if (!isset($annotations[ChildProcess::class])) {
-        return false;
-    }
-
-    return true;
+    return toXOrNotToX(ChildProcess::class, $callable, $annotationReader);
 }
